3caf8bc4de78abb3421566b8470a71790fff2389
H-akim <92632018+H-akim@users.noreply.github.com>
2023-07-04 15:27:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var search_box = document.getElementById("search_box");
+var google_icon = document.getElementById("google_icon");
+
+google_icon.onclick = function(){
+    search_box.classList.toggle("active")
+}
